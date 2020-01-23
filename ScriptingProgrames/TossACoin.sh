b36acee03c14d0e02c9fb96f1d1f0b102de7b470
janardan1995 <44210489+janardan1995@users.noreply.github.com>
2020-01-23 12:10:30 +0530

    [JD]Add.LeapYear, Toss A coin, Replace a string in shell programes are added to the remote repository by creating a branch named algorithmScripting

---
+++ ScriptingProgrames/TossACoin.sh
@@ -0,0 +1,30 @@
+
+headCount=0
+tailCount=0
+echo "enter how many times you wanna toss the coin"
+read t
+toss=$t
+
+while [ $toss -gt 0 ]
+do
+a=$(($RANDOM%2))
+echo "....$a"
+if [ $a -eq 1 ]
+then
+         headCount=$(($headCount+1))
+fi
+if [ $a -eq 0 ]
+then
+         tailCount=$(($tailCount+1))
+fi
+toss=$(($toss-1))
+done
+
+echo ".............................."
+echo "tailCount=$tailCount"
+echo "headCount=$headCount"
+per=100
+a=$(($headCount * 100/t))
+b=$(($tailCount * 100/t))
+
+echo "percentage of head: $a%  and tail: $b%"
