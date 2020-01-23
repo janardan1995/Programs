7dcabdf71668c4f9628c4d3a6a8fb701e779c683
janardan1995 <44210489+janardan1995@users.noreply.github.com>
2020-01-23 09:09:50 +0530

    [Janardan]Add.Arithematic programs to the remote repository by creating a brance as ArithematicProgramming

---
+++ ArithematicProgramming/Algorithm/Algorithm/regexxxxxxxxx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Algorithm
+{
+    class regexxxxxxxxx
+    {
+        public static void reee()
+        {
+                Console.WriteLine("enter DOB");
+            String fullName = Console.ReadLine();
+
+
+            string strRegex = @"[0-9]{1}[1-2]{1}/[0-9]{1}[1-2]{1}/[1-2]{1}[0-9]{3}";
+            Regex re = new Regex(strRegex);
+
+            if (re.IsMatch(fullName)) {
+                Console.WriteLine("yes i found it");
+            }
+            else
+            {
+                Console.WriteLine("invalide try again");
+              
+            }
+            string str = "hello janardan";
+            string[] arr = str.Split(" "); 
+            Console.WriteLine("{0}",arr[0]);
+
+        }
+    }
+}
